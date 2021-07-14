8c67b7b1df9d3b886844491d122a961e12565bb6
dennywachira <denniz.wachira@gmail.com>
2021-07-14 10:25:50 +0300

    Create quiz4.py

---
+++ Assignments/Lab 03/quiz4.py
@@ -0,0 +1,9 @@
+
+y=int(input("enter an integer 1\n"))
+x=int(input("enter an integer 2\n"))
+z=int(input("enter an integer 3\n"))
+
+if (y==x and x==z):
+    print("Equal")
+else:
+    print("Not Equal")
