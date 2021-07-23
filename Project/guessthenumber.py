2872c07369cf228d6baf602d0def22ed4421303e
dennywachira <denniz.wachira@gmail.com>
2021-07-23 07:40:15 +0300

    Update guessthenumber.py

--- Project/guessthenumber.py
+++ Project/guessthenumber.py
@@ -3,26 +3,23 @@ import random
 number= random.randint(0,10)
 attempts=0
 score=0
-
+print("WELCOME to our guessing Game!\nYou will guess a number between (0 and 10),if it's correct you win and if its wrong we offer you 5 attempts.")
+print("Lets start playing!!!!")
 
 while attempts <5:
-    userNum=int(input("Enter a Number\n"))
+    userNum=int(input("Guess a Number\n"))
     attempts=attempts+1
     if userNum<number:
-        print("your guess is too low")
+        print("your guess is too low try a higher Number")
     if userNum>number:
-        print("your guess is too high")
+        print("your guess is too high try a lower number")
     if userNum==number:
         break
 if userNum==number:
-    score=+1
+    score=score+1
     print("Congratulations You Won!")
     print("You have Scored:"+"" + str(score)+" "+"points")
-    goahead=int(input("Do you wish to Continue Playing? if Yes enter 1,if Not enter 0\n"))
-    if goahead==1:
-        print("welcome back")
-    else:
-        print("good bye")
+    
 else:
     print(" OOPS!! You lost after"+" " + str(attempts)+" "+ "attempts")
 
